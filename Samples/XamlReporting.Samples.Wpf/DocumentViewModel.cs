5811a7db0a75f0601f2e3036688a6ca5edc1d84f
David Neumann <david.neumann@live.com>
2016-02-04 20:58:45 +0100

    Cleaned up the WPF sample application.

--- Samples/XamlReporting.Samples.Wpf/DocumentViewModel.cs
+++ Samples/XamlReporting.Samples.Wpf/DocumentViewModel.cs
@@ -1,14 +1,28 @@
 ﻿
 namespace XamlReporting.Samples.Wpf
 {
+    /// <summary>
+    /// Represents the view model for the document that is to be created.
+    /// </summary>
     public class DocumentViewModel
     {
-        public string FrontPageText { get; } = "Hello, World!";
+        #region Public Properties
 
-        public string PageHeader { get; } = "Much Header";
+        /// <summary>
+        /// Gets or sets the title of the document.
+        /// </summary>
+        public string Title { get; set; } = "Hello World from WPF";
 
-        public string PageFooter { get; } = "Such Footer";
+        /// <summary>
+        /// Gets or sets the abstract of the document.
+        /// </summary>
+        public string Abstract { get; set; } = "This is a simple test of the XAML Reporting engine in a WPF application.";
 
-        public string IntroductoryText { get; } = "So Paper, much report, so render, WOW!";
+        /// <summary>
+        /// Gets or sets the name of the author.
+        /// </summary>
+        public string Author { get; set; } = "Jane Doe";
+
+        #endregion
     }
 }
