19c64ed4127d1f3c3d1ff56bb2965b46ecc90d37
David Neumann <david.neumann@live.com>
2016-03-04 17:14:35 +0100

    Bumped up the version number and made the project ready for publishing on NuGet.

--- System.Windows.Documents.Reporting/Properties/AssemblyInfo.cs
+++ System.Windows.Documents.Reporting/Properties/AssemblyInfo.cs
@@ -27,7 +27,7 @@ using System.Runtime.InteropServices;
 
 #region Assembly Version
 
-[assembly: AssemblyVersion("0.1.4.*")]
-[assembly: AssemblyFileVersion("0.1.4.0")]
+[assembly: AssemblyVersion("0.1.5.*")]
+[assembly: AssemblyFileVersion("0.1.5.0")]
 
 #endregion
